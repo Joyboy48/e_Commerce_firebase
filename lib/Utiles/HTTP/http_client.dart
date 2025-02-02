322106e699316bada8d663508a140fe53c7470e4
Astitva Arya <147731572+Joyboy48@users.noreply.github.com>
2025-02-02 13:58:55 +0530

    Update http_client.dart

--- lib/Utiles/HTTP/http_client.dart
+++ lib/Utiles/HTTP/http_client.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class HttpHelper {
-  static const String _baseUrl = 'http://10.0.2.2:8000/api/v1/users';
+  static const String _baseUrl = '-_-';
 
   //register
   static Future<Map<String, dynamic>> register(
